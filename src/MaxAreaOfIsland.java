351fd8ad6aef870aa4f2b6e9ef06cc623755bf89
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2022-08-06 20:45:38 -0700

    added the comments, please refer to it for the better understanding

--- src/MaxAreaOfIsland.java
+++ src/MaxAreaOfIsland.java
@@ -3,6 +3,9 @@ public class MaxAreaOfIsland {
    static int[] dx={-1,1,0,0};
    static int[] dy={0,0,-1,1};
 
+   //if getting out of the grid then return 0, otherwise, update the grid with 0, which acts as a wall
+    // and now check all the four possible directions to traverse.
+    // increment the count with every one found, then return count
     static int dfs(int i, int j, int n, int m, int[][]grid){
         if(i<0 || j<0 || i== n || j==m || grid[i][j]==0){
             return 0;
@@ -21,6 +24,8 @@ public class MaxAreaOfIsland {
 
 
 
+    //get row and column of grid and traverse, if find 1, then return result with dfs,
+    //whichever result is greatest, return that at the end
     public static int maxAreaOfIsland(int [][]grid){
         int n = grid.length;
         int m = grid[0].length;
