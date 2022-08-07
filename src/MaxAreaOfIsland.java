38cfd18a50961244e9edb23080e7930e0a198a49
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2022-08-06 20:35:47 -0700

    coded a dfs method which return the count, count is basically the number of the 1s found consecutively

--- src/MaxAreaOfIsland.java
+++ src/MaxAreaOfIsland.java
@@ -1,7 +1,44 @@
 public class MaxAreaOfIsland {
 
+   static int[] dx={-1,1,0,0};
+   static int[] dy={0,0,-1,1};
 
-    
+    static int dfs(int i, int j, int n, int m, int[][]grid){
+        if(i<0 || j<0 || i== n || j==m || grid[i][j]==0){
+            return 0;
+        }
+        grid[i][j]=0;
+
+        int count =0;
+        for(int k=0; k<4; k++){
+            int nx = i + dx[k];
+            int ny = j+ dy[k];
+
+            count += dfs(nx,ny,n, m,grid);
+
+        }return count;
+
+
+
+    }
+
+
+
+    public static int maxAreaOfIsland(int [][]grid){
+        int n = grid.length;
+        int m = grid[0].length;
+        int max = 0;
+
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+
+                if(grid[i][j]==1){
+                int result = dfs(i, j,n, m, grid);
+                max = Math.max(max,result);
+            }
+            }
+        }return max;
+    }
 
 
 
